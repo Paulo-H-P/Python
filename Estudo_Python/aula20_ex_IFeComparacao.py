8aa6f000e0c7eec7799766c651e1e483da9499ac
Paulo-H-P <fredysoh@gmail.com>
2024-01-07 00:59:22 -0300

    07/01/24

---
+++ Estudo_Python/aula20_ex_IFeComparacao.py
@@ -0,0 +1,10 @@
+numero1 = input('digite um numero :')
+numero2 = input('digite outro numero :')
+
+num1 = int(numero1)
+num2 = int(numero2)
+
+if num1 > num2:
+    print(f'O primeiro valor: {num1} é maior que o segundo valor: {num2}')
+else:
+    print(f'O segundo valor: {num2} é maior que o primeiro valor: {num1}')
---
+++ Estudo_Python/aula21_OperadoresLogicos.py
@@ -0,0 +1,7 @@
+'''
+Operadores Logicos
+and(e) or(ou) not(não)
+and - todas as condições precisam ser vedadeiras , se um valor for falso a condição é invalidada
+
+
+'''
